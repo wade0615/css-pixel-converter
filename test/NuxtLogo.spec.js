f2724c494e8aa27e7dda6fffae2be2952dabdf3d
WadePro <wsw0615@gmail.com>
2021-09-07 18:01:24 +0800

    feat: seccess run test

--- test/NuxtLogo.spec.js
+++ test/NuxtLogo.spec.js
@@ -1,4 +1,5 @@
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
+import index from '@/pages/index.vue'
 import NuxtLogo from '@/components/NuxtLogo.vue'
 
 describe('NuxtLogo', () => {
@@ -6,4 +7,14 @@ describe('NuxtLogo', () => {
     const wrapper = mount(NuxtLogo)
     expect(wrapper.vm).toBeTruthy()
   })
-})
+});
+
+describe('index page', () => {
+  test('ppi', () => {
+    expect(index.data().ppi).toBe('');
+  })
+  test('getPixelDensity()', () => {
+    expect(index.methods.getPixelDensity(320)).toBe(2);
+    expect(index.methods.getPixelDensity(480)).toBe(3);
+  })
+});
